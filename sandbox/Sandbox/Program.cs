3c1435e5b97e5a57f29f5b296ff8ff890defec43
scottshumway925 <shumworld925@gmail.com>
2025-04-24 21:51:32 -0600

    HW Change

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("Please Input Your First Name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Please Input Your Last Name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}");
     }
 }
