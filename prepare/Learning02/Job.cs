4c03325ad01e7e3f0f50a9c0dee89158016f0266
scottshumway925 <shumworld925@gmail.com>
2025-05-07 13:13:35 -0600

    Fixed the output to be cleaner

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,9 +5,6 @@ public class Job {
     public int _endYear;
 
     public void Display() {
-        Console.WriteLine($"\nThe job title is: {_jobTitle}");
-        Console.WriteLine($"The company is: {_company}");
-        Console.WriteLine($"The start year is: {_startYear}");
-        Console.WriteLine($"The end year is: {_endYear}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -4,11 +4,9 @@ public class Resume {
 
     public void Display() {
         Console.WriteLine($"\nThis is {_name}'s resume: ");
+        Console.WriteLine("Jobs: ");
         foreach (Job job in _jobs) {
-            Console.WriteLine($"\nThe job title is: {job._jobTitle}");
-            Console.WriteLine($"The company is: {job._company}");
-            Console.WriteLine($"The start year is: {job._startYear}");
-            Console.WriteLine($"The end year is: {job._endYear}");
+            Console.WriteLine($"{job._jobTitle} ({job._company}) {job._startYear}-{job._endYear}");
         }
     }
 }
