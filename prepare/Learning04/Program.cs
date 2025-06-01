04f677a600c7e066f17fda97a40254a9bd55320f
scottshumway925 <shumworld925@gmail.com>
2025-05-31 20:48:34 -0600

    Inheritance Prep

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("John", "Derivitives");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment a1 = new MathAssignment("John", "Math", "7.3", "8-19");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine(a1.GetHomeworkList());
+
+        WritingAssignment a2 = new WritingAssignment("John", "History", "George Washingtons Ascent");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetWritingInformation());
     }
 }
