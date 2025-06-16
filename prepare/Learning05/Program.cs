36fec5c8251e52fdfae3d470e281a289decadfd6
scottshumway925 <shumworld925@gmail.com>
2025-06-16 17:52:27 -0600

    Polymorphism Practice

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Rectangle rectangle1 = new Rectangle("Pink", 2, 3);
+        Rectangle rectangle2 = new Rectangle("Brown", 4, 5);
+        Square square1 = new Square("Chartrouse", 7);
+        Square square2 = new Square("Blue", 5);
+        Circle circle1 = new Circle("Orange", 4);
+        Circle circle2 = new Circle("White", 7);
+
+        List<Shape> shapeList = new List<Shape>() {rectangle1, rectangle2, square1, square2, circle1, circle2};
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.ToString());
+            Console.WriteLine($"Your shape color is: {shape.GetColor()} \nWith an area of: {shape.GetArea()}\n");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,14 @@
+public class Square : Shape
+{
+    private double _sideLength;
+
+    public Square(string color, double sideLength) : base(color)
+    {
+        _sideLength = sideLength;
+    }
+
+    public override double GetArea()
+    {
+        return _sideLength * _sideLength;
+    }
+}
